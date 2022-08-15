ad39bd1e720dc41e9baae1101688fa3e0933482f
Leonardo Furmann <leonardohfurmann@gmail.com>
2022-08-15 12:13:55 -0300

    aula 1

---
+++ Ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double real = 0;
+            Console.WriteLine("Digite um valor em real: ");
+            real = Convert.ToDouble(Console.ReadLine());
+
+            ConverterDolar(real);
+            ConverterPeso(real);
+            ConverterEuro(real);
+
+
+            static void ConverterDolar(double real)
+            {
+                Console.Write("\nConversão para Euro: " + (real / 6.14));
+            }
+
+            static void ConverterPeso(double real)
+            {
+                Console.Write("\nConversão para Peso: " + (real / 0.65));
+            }
+
+            static void ConverterEuro(double real)
+            {
+                Console.Write("\nConversão para Dolar: " + (real / 5.17));
+            }
+        }
+    }
+}
---
+++ Hello/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double largura = 0;
+            double altura = 0;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Digite uma largura:");
+                    largura = Convert.ToDouble(Console.ReadLine());
+                    break;
+
+                }
+                catch (Exception erro)
+                {
+                    // Console.WriteLine(erro.Message);
+                    Console.WriteLine("Largura inválida");
+                }
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Digite uma altura:");
+                    altura = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch (Exception erro)
+                {
+                    Console.WriteLine("Altura inválida");
+                }
+
+            }
+
+            Console.WriteLine("A area é " + CalculaArea(largura, altura));
+        }
+
+        static public double CalculaArea(double largura, double altura)
+        {
+            return largura * altura;
+        }
+    }
+}
+
+
+
