2ac45d32f921ddfb524d04ecd9cfd8c5bfc60f43
Leonardo Furmann <leohitnerf@gmail.com>
2022-09-17 17:17:18 -0300

    alterações

--- ExNumero/Program.cs
+++ ExNumero/Program.cs
@@ -10,15 +10,52 @@ namespace Ex2
     static void Main(string[] args)
     {
       
-        int soma =0 ;
-        Console.WriteLine("Digite um número");
+        double soma =0 ;
+        Console.WriteLine("Digite um número: ");
         String numero = Console.ReadLine();
 
         char[] n = numero.ToCharArray();
 
-        for (int i = 0; i < n.Length; i++)
+        for (int i = n.Length; i >- 0 ; i++)
         {
-            soma += n * (Math.Pow(10,i));
+        
+        for (int j = 0; j < n.Length; j++)
+        {
+          
+        
+          switch(n[i]){
+            case '1':
+            soma += 1 * (Math.Pow(10,i));
+            break;
+            case '2':
+            soma += 2 * (Math.Pow(10,i));
+            break;
+            case '3':
+            soma += 3 * (Math.Pow(10,i));
+            break;
+            case '4':
+            soma += 4 * (Math.Pow(10,i));
+            break;
+            case '5':
+            soma += 5 * (Math.Pow(10,i));
+            break;
+            case '6':
+            soma += 6 * (Math.Pow(10,i));
+            break;
+            case '7':
+            soma += 7 * (Math.Pow(10,i));
+            break;
+            case '8':
+            soma += 8 * (Math.Pow(10,i));
+            break;
+            case '9':
+            soma += 9 * (Math.Pow(10,i));
+            break;
+            case '0':
+            soma += (Math.Pow(10,i));
+            break;
+            }
+           }
         }
 
     Console.WriteLine(soma);
--- Teste/Program.cs
+++ Teste/Program.cs
@@ -10,34 +10,92 @@ namespace Trabalho
 			{
 				var baseUsuarios = new BaseUsuarios("users.txt");
 				
-				baseUsuarios.Limpar();
+				Console.WriteLine("Bem vind@!");
 				
-				Console.WriteLine("limpei a base de usuarios:");
-				Console.WriteLine(baseUsuarios.Serializar());
-								
-				var x = new Usuario("Huguinho", "huguinho@email.com", "senha123"  );
-				var y = new Usuario("Zezinho" , "zezinho@email.com" , "senha321"  );
-				var z = new Usuario("Luizinho", "luizinho@email.com", "minhaSenha");
-				
-				baseUsuarios.AdicionarUsuario(x);
-				baseUsuarios.AdicionarUsuario(y);
-				baseUsuarios.AdicionarUsuario(z);
-				
-				Console.WriteLine("adicionei huguinho, zezinho e luizinho:");
-				Console.WriteLine(baseUsuarios.Serializar());
-				
-				var huguinho = baseUsuarios.BuscarPorEmail("huguinho@email.com");
-				baseUsuarios.RemoverUsuario(huguinho);
-				
-				Console.WriteLine("removi huguinho:");
-				Console.WriteLine(baseUsuarios.Serializar());
-								
-				var zezinho = baseUsuarios.BuscarPorEmail("luizinho@email.com");
-				zezinho.SetNome("Luizao");
-				baseUsuarios.AtualizarUsuario(zezinho);
-				
-				Console.WriteLine("atualizei nome do luizinho:");
-				Console.WriteLine(baseUsuarios.Serializar());
+				bool sair = false;
+				while(!sair)
+				{
+					Console.WriteLine("Digite 1 para listar os usuarios");
+					Console.WriteLine("Digite 2 para cadastrar um usuario");
+					Console.WriteLine("Digite 3 para atualizar um usuario");
+					Console.WriteLine("Digite 4 para remover um usuario");
+					Console.WriteLine("Digite 5 para sair");
+					
+					int opcao = 0;
+					
+					try
+					{
+						opcao = int.Parse(Console.ReadLine());
+					}
+					catch(Exception erro)
+					{
+					}
+					
+					switch(opcao)
+					{
+						case 1:
+							
+							Console.WriteLine(baseUsuarios.Serializar());
+							break;
+						
+						case 2:
+							
+							{
+								Console.WriteLine("Insira o nome do usuario");
+								var username = Console.ReadLine();
+								Console.WriteLine("Insira o email do usuario");
+								var email = Console.ReadLine();
+								Console.WriteLine("Insira uma senha");
+								var password = Console.ReadLine();
+								Console.WriteLine("Confirme a senha");
+								var confirmacao = Console.ReadLine();
+								if(password != confirmacao)
+								{
+									Console.WriteLine("Senha nao confere");
+									break;
+								}
+								else
+								{
+									var usuario = new Usuario(username, email, password);
+									baseUsuarios.AdicionarUsuario(usuario);
+								}
+							}
+							break;
+						
+						case 3:
+							
+							//TODO atualizar usuario
+							Console.WriteLine("Desculpe, esta opcao ainda nao foi implementada =/");
+							break;
+						
+						case 4:
+							
+							{
+								Console.WriteLine("Insira o email do usuario que deseja remover");
+								var email = Console.ReadLine();
+								try
+								{
+									var usuario = baseUsuarios.BuscarPorEmail(email);
+									baseUsuarios.RemoverUsuario(usuario);
+								}
+								catch(Exception erro)
+								{
+									Console.WriteLine("Nao foi possivel remover o usuario " + email);
+								}
+							}
+							break;
+						
+						case 5:
+							sair = true;
+							break;
+							
+						default:
+							Console.WriteLine("Opcao invalida! Tente novamente...");
+							break;
+					}
+					
+					Console.WriteLine();
+				}
 				
 				baseUsuarios.Salvar();
 			}
